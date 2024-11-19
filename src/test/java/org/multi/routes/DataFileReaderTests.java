1ee7cd1b9d4d5f97e549063cf03fc2142785aa9f
pinkblink <m.pink.blink@gmail.com>
2024-11-19 13:51:08 +0100

    refactor

--- src/test/java/org/multi/routes/DataFileReaderTests.java
+++ src/test/java/org/multi/routes/DataFileReaderTests.java
@@ -23,9 +23,33 @@ public class DataFileReaderTests {
 
     @BeforeTest
     public void setUp() {
-        expectedPassengersList = Arrays.asList("passenger: 1", "passenger: 2", "passenger: 3");
-        expectedBusesList = Arrays.asList("number: 1", "number: 2", "number: 3");
-        expectedBusStopsList = Arrays.asList("stop: 1", "stop: 2", "stop: 3");
+        expectedPassengersList = Arrays.asList("passenger: James Anderson",
+                "passenger: Emily Parker",
+                "passenger: Michael Bennett",
+                "passenger: Sophia Harris",
+                "passenger: William Carter",
+                "passenger: Olivia Morgan",
+                "passenger: Alexander Scott",
+                "passenger: Charlotte Thompson",
+                "passenger: Benjamin Wright",
+                "passenger: Amelia Collins");
+
+        expectedBusesList = Arrays.asList("bus_number: 1 max_passengers: 2",
+                "bus_number: 2 max_passengers: 4",
+                "bus_number: 3 max_passengers: 1",
+                "bus_number: 4 max_passengers: 1",
+                "bus_number: 5 max_passengers: 2");
+
+        expectedBusStopsList = Arrays.asList("bus_stop: 1 max_buses: 2",
+                "bus_stop: 2 max_buses: 3",
+                "bus_stop: 3 max_buses: 1",
+                "bus_stop: 4 max_buses: 1",
+                "bus_stop: 5 max_buses: 3",
+                "bus_stop: 6 max_buses: 4",
+                "bus_stop: 7 max_buses: 4",
+                "bus_stop: 8 max_buses: 4",
+                "bus_stop: 9 max_buses: 2",
+                "bus_stop: 10 max_buses: 2");
     }
 
     @Test
